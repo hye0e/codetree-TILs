2b64f4eeeae1b4f1760291921d1ec14496e9d3ec
HyeYoung Shin <nanda5050@naver.com>
2024-07-27 20:15:52 +0900

    Title: 문자열 정렬, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/문자열 정렬/string-sort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        char[] charArr = input.toCharArray();
+        Arrays.sort(charArr);
+        System.out.println(new String(charArr));
+    }
+}
