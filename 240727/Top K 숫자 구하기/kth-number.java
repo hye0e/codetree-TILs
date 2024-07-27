71e39ff79297fba23072d92800fabbd8752fcdf3
HyeYoung Shin <nanda5050@naver.com>
2024-07-27 21:04:46 +0900

    Title: Top K 숫자 구하기, Time: 163ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240727/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[K - 1]);
+    }
+}
