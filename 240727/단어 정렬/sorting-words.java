bf0c9da323a0e3b8a7e1b92c637e747351eeb467
HyeYoung Shin <nanda5050@naver.com>
2024-07-27 20:23:50 +0900

    Title: 단어 정렬, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

--- 240727/단어 정렬/sorting-words.java
+++ 240727/단어 정렬/sorting-words.java
@@ -1,20 +1,20 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // 여기에 코드를 작성해주세요.(
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        String[] arr = new String[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        String[] sArr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            sArr[i] = br.readLine();
         }
 
-        Arrays.sort(arr);
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i]).append("\n");
+        Arrays.sort(sArr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(sArr[i]);
         }
-        System.out.println(sb);
     }
 }
