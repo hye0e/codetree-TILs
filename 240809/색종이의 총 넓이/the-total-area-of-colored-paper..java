f3c3eb125b6e8d99b4a3ce659b766955cfe76ae5
HyeYoung Shin <nanda5050@naver.com>
2024-08-09 00:05:19 +0900

    Title: 색종이의 총 넓이, Time: 420ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240809/색종이의 총 넓이/the-total-area-of-colored-paper..java
+++ 240809/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -6,13 +6,14 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] graph = new int[200][200];
+        int[][] graph = new int[10000][10000];
+        int offset = 100; 
         for (int k = 0; k < N; k++) {
             String[] input = br.readLine().split(" ");
-            int x1 = Integer.parseInt(input[0]);
-            int y1 = Integer.parseInt(input[1]);
-            int x2 = x1 + 8;
-            int y2 = y1 + 8;
+            int x1 = Integer.parseInt(input[0]) + offset;
+            int y1 = Integer.parseInt(input[1]) + offset;
+            int x2 = x1 + 8 + offset;
+            int y2 = y1 + 8 + offset;
             for (int i = x1; i < x2; i++) {
                 for (int j = y1; j < y2; j++) {
                     graph[i][j] = 1;
