97b51db2131b90819ddc806660913ddbe8961519
HyeYoung Shin <nanda5050@naver.com>
2024-08-25 15:48:40 +0900

    Title: 씨 오 더블유 2, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240825/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[] arr = br.readLine().toCharArray();
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if (arr[i] == 'C' && arr[j] == 'O' && arr[k] == 'W') {
+                        cnt++;
+                    }                    
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
